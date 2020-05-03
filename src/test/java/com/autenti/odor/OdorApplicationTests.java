8088220f52443bd6e0e3023e42344d73e23d1ed0
kuba_chmielewski <jakub.chmielewski@autenti.com>
2020-05-03 21:03:51 +0200

    init

---
+++ src/test/java/com/autenti/odor/OdorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autenti.odor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
